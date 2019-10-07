4e6b111f832ea97419bf7374307996db0779d305
Shuwen Zheng <55172213+SUE7EN@users.noreply.github.com>
2019-10-07 12:23:28 -0700

    Update angle-lines.js

--- angle-lines.js
+++ angle-lines.js
@@ -6,7 +6,7 @@ function setup() {
 function draw() {
   background(255);
 
-  for (var x = 10; x < 399; x = x + 10) {
+  for (var x = 100; x <= width - 10; x = x + 10) {
     line(x, height/2, x+100, height/2-75);
   }
 }
